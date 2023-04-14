78239a29c2d3c692aa2704104cbf2c387325bad9
Кира Максимова 211-323 <immweasel@yandex.ru>
2023-04-14 14:54:31 +0300

    fix: исправить форму редактирования (теперь данные со страницы есть при открытии формы)

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,17 @@
 const nameObject = document.querySelector('.profile__name'); //имя профиля
 const descriptionObject = document.querySelector('.profile__description'); //описание профиля
-
-const nameText = nameObject.textContent; //задаем и получаем текстовое содержимое
-const descriptionText = descriptionObject.textContent;
-
 const editObject = document.querySelector('.profile__edit') //кнопка для редактирования профиля
 const popupObject = document.querySelector('.popup') //форма редактирования
 const closeObject = document.querySelector('.popup__close') //кнопка закрытия формы редактирования
-
 const textNameObject = document.querySelector('#name') //инпут имени профиля
 const textDescriptionObject = document.querySelector('#description') //инпут описания профиля
 const formObject = document.querySelector('.popup__form') //кнопка для сохранения изменений
 
 function openPopup() {
+	const nameText = nameObject.textContent; //задаем и получаем текстовое содержимое
+	const descriptionText = descriptionObject.textContent;
+	textNameObject.value = nameText;
+	textDescriptionObject.value = descriptionText;
   popupObject.classList.add('popup_opened');
 }
 
