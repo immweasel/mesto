e8fc0040bc68fa8266a8723cb42aee44f914efbe
Кира Максимова 211-323 <immweasel@yandex.ru>
2023-04-26 15:02:24 +0300

    feat: сделать добавление новых карточек

--- scripts/index.js
+++ scripts/index.js
@@ -2,30 +2,96 @@ const nameObject = document.querySelector(".profile__name"); //имя профи
 const descriptionObject = document.querySelector(".profile__description"); //описание профиля
 const editObject = document.querySelector(".profile__edit"); //кнопка для редактирования профиля
 const popupObject = document.querySelector(".popup"); //форма редактирования
-const closeObject = document.querySelector(".popup__close"); //кнопка закрытия формы редактирования
-const textNameObject = document.querySelector("#name"); //инпут имени профиля
-const textDescriptionObject = document.querySelector("#description"); //инпут описания профиля
-const formObject = document.querySelector(".popup__form"); //кнопка для сохранения изменений
+const closeEditObject = document.querySelector(".popup__close_edit"); //кнопка закрытия формы редактирования
+const closeAddObject = document.querySelector(".popup__close_add"); //кнопка закрытия формы добавления
+let textNameObject = document.querySelector("#name"); //инпут имени профиля
+let textDescriptionObject = document.querySelector("#description"); //инпут описания профиля
+let placeNameObject = document.querySelector("#place-name"); //импут названия места
+let placeLinkObject = document.querySelector("#place-link"); //импут ссылки на картинку места
+const formEditObject = document.querySelector(".popup__form_edit"); 
+const formAddObject = document.querySelector(".popup__form_add"); 
+const cardTemplate = document.querySelector("#card-template").content;
+const editPopup = document.querySelector(".popup__container_edit");
+const addPopup = document.querySelector(".popup__container_add");
+const addButton = document.querySelector(".profile__add");
+const photoGrid = document.querySelector(".photo-grid");
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
 
-function openPopup() {
+function addCard(name, link) {
+  const firstCard = cardTemplate.querySelector(".photo-grid__item").cloneNode(true);
+  firstCard.querySelector(".photo-grid__description").textContent = name;
+  firstCard.querySelector(".photo-grid__photo").src = link;
+  firstCard.querySelector(".photo-grid__photo").alt = name;
+  photoGrid.prepend(firstCard);
+}
+
+initialCards.forEach(element => addCard(element.name, element.link));
+
+function openPopupOverlay() {
+  popupObject.classList.add("popup_opened");
+}
+
+function openEditPopup() {
+  openPopupOverlay();
+  editPopup.classList.add("popup__container_opened");
   const nameText = nameObject.textContent; //задаем и получаем текстовое содержимое
   const descriptionText = descriptionObject.textContent;
   textNameObject.value = nameText;
   textDescriptionObject.value = descriptionText;
-  popupObject.classList.add("popup_opened");
+}
+
+function openAddPopup() {
+  openPopupOverlay();
+  addPopup.classList.add("popup__container_opened");
 }
 
 function closePopup() {
   popupObject.classList.remove("popup_opened");
+  editPopup.classList.remove("popup__container_opened");
+  addPopup.classList.remove("popup__container_opened");
 }
 
-function popupSubmit(evt) {
+function popupAddSubmit(evt) {
+  evt.preventDefault();
+  addCard(placeNameObject.value, placeLinkObject.value);
+  closePopup();
+}
+
+function popupEditSubmit(evt) {
   evt.preventDefault();
   nameObject.textContent = textNameObject.value;
   descriptionObject.textContent = textDescriptionObject.value;
   closePopup();
 }
 
-editObject.addEventListener("click", openPopup);
-closeObject.addEventListener("click", closePopup);
-formObject.addEventListener("submit", popupSubmit);
+editObject.addEventListener("click", openEditPopup);
+addButton.addEventListener("click", openAddPopup);
+closeAddObject.addEventListener("click", closePopup);
+closeEditObject.addEventListener("click", closePopup);
+formAddObject.addEventListener("submit", popupAddSubmit);
+formEditObject.addEventListener("submit", popupEditSubmit);
